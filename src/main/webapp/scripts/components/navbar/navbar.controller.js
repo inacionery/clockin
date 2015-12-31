d3f12d84628cd5702119930a94f2de2dd79ebce7
Clovis Neto <cloves13@hotmail.com>
2015-12-31 19:51:31 -0300

    Edit clocks style

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,23 +1,6 @@
 'use strict';
 
 angular.module('clockinApp')
-.filter('keyboardShortcut', function($window) {
-    return function(str) {
-      if (!str) return;
-      var keys = str.split('-');
-      var isOSX = /Mac OS X/.test($window.navigator.userAgent);
-      var seperator = (!isOSX || keys.length > 2) ? '+' : '';
-      var abbreviations = {
-        M: isOSX ? '⌘' : 'Ctrl',
-        A: isOSX ? 'Option' : 'Alt',
-        S: 'Shift'
-      };
-      return keys.map(function(key, index) {
-        var last = index == keys.length - 1;
-        return last ? key : abbreviations[key];
-      }).join(seperator);
-    };
-  })
     .controller('NavbarController', function ($scope, $mdDialog, $location, $state, Auth, Principal, ENV) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
@@ -27,20 +10,4 @@ angular.module('clockinApp')
             Auth.logout();
             $state.go('login');
         };
-
-         this.settings = {
-	      printLayout: true,
-	      showRuler: true,
-	      showSpellingSuggestions: true,
-	      presentationMode: 'edit'
-	    };
-
-	    this.sampleAction = function(name, ev) {
-	      $mdDialog.show($mdDialog.alert()
-	        .title(name)
-	        .textContent('You triggered the "' + name + '" action')
-	        .ok('Great')
-	        .targetEvent(ev)
-	      );
-	    };
-    });
+  });
