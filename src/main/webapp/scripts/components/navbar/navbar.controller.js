ff55fec6fed0c829b0f3ef825e563708319ad9a1
Clovis Neto <cloves13@hotmail.com>
2015-12-31 16:48:45 -0300

    Create intranet layout

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -1,7 +1,24 @@
 'use strict';
 
 angular.module('clockinApp')
-    .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV) {
+.filter('keyboardShortcut', function($window) {
+    return function(str) {
+      if (!str) return;
+      var keys = str.split('-');
+      var isOSX = /Mac OS X/.test($window.navigator.userAgent);
+      var seperator = (!isOSX || keys.length > 2) ? '+' : '';
+      var abbreviations = {
+        M: isOSX ? '⌘' : 'Ctrl',
+        A: isOSX ? 'Option' : 'Alt',
+        S: 'Shift'
+      };
+      return keys.map(function(key, index) {
+        var last = index == keys.length - 1;
+        return last ? key : abbreviations[key];
+      }).join(seperator);
+    };
+  })
+    .controller('NavbarController', function ($scope, $mdDialog, $location, $state, Auth, Principal, ENV) {
         $scope.isAuthenticated = Principal.isAuthenticated;
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
@@ -10,4 +27,20 @@ angular.module('clockinApp')
             Auth.logout();
             $state.go('login');
         };
+
+         this.settings = {
+	      printLayout: true,
+	      showRuler: true,
+	      showSpellingSuggestions: true,
+	      presentationMode: 'edit'
+	    };
+
+	    this.sampleAction = function(name, ev) {
+	      $mdDialog.show($mdDialog.alert()
+	        .title(name)
+	        .textContent('You triggered the "' + name + '" action')
+	        .ok('Great')
+	        .targetEvent(ev)
+	      );
+	    };
     });
