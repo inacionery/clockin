3c303180ad273c94c0ae1de03ec45eeae2d7efd1
Inacio Nery <inacio.nery@liferay.com>
2016-04-29 08:35:54 -0300

    Remove EN language

--- src/main/webapp/app/components/language/language.constants.js
+++ src/main/webapp/app/components/language/language.constants.js
@@ -9,7 +9,6 @@
          They are written in English to avoid character encoding issues (not a perfect solution)
          */
         .constant('LANGUAGES', [
-            'en',
             'pt-br'
             // jhipster-needle-i18n-language-constant - JHipster will add/remove languages in this array
         ]
