0337e4bb53f84dc68e297b30cf78e2d636ef14fa
Inacio Nery <inacio.nery@liferay.com>
2016-04-30 09:32:17 -0300

    Translate to pt-br the pagination message

--- src/main/webapp/app/components/util/jhi-item-count.directive.js
+++ src/main/webapp/app/components/util/jhi-item-count.directive.js
@@ -3,9 +3,9 @@
 
     var jhiItemCount = {
         template: '<div class="info">' +
-                    'Showing {{(($ctrl.page-1) * 20)==0 ? 1:(($ctrl.page-1) * 20)}} - ' +
+                    'Exibindo {{(($ctrl.page-1) * 20)==0 ? 1:(($ctrl.page-1) * 20)}} - ' +
                     '{{($ctrl.page * 20) < $ctrl.queryCount ? ($ctrl.page * 20) : $ctrl.queryCount}} ' +
-                    'of {{$ctrl.queryCount}} items.' +
+                    'de {{$ctrl.queryCount}} resultados.' +
                 '</div>',
         bindings: {
             page: '<',
