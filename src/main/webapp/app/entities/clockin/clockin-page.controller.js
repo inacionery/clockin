5aa5f941b1ed4a0f33550de23eb5b5e01520075d
Inácio Nery <inacio.nery@liferay.com>
2016-05-04 18:04:09 -0300

    Fix navigation

--- src/main/webapp/app/entities/clockin/clockin-page.controller.js
+++ src/main/webapp/app/entities/clockin/clockin-page.controller.js
@@ -9,6 +9,22 @@
 
     function ClockinPageController($scope, $rootScope, $stateParams, entity, Clockin, Employee) {
         var vm = this;
+        var month = eval($stateParams.month);
+        var year = eval($stateParams.year);
+        vm.previousYear = year;
+        vm.previousMonth = (month - 1);
+        vm.nextYear = year;
+        vm.nextMonth = (month + 1);
+        if (month == 1){
+        	vm.previousYear = (year - 1);
+        	vm.previousMonth = 12;
+        } else if (month == 12){
+        	 vm.nextYear = (year + 1);
+             vm.nextMonth = 1;
+        }
+        vm.date = new Date(vm.previousYear, vm.previousMonth);
+        vm.previousDate = new Date(vm.previousYear, vm.previousMonth - 1);
+        vm.nextDate = new Date(vm.nextYear, vm.nextMonth -1);
         vm.workdays = entity;
     }
 })();
--- src/main/webapp/app/entities/clockin/clockin.controller.js
+++ src/main/webapp/app/entities/clockin/clockin.controller.js
@@ -10,6 +10,23 @@
     function ClockinController ($scope, $state, Clockin, ClockinSearch) {
         var vm = this;
         vm.workdays = [];
+        var today = new Date();
+        var month = eval(today.getMonth())+1;
+        var year = eval(today.getFullYear());
+        vm.previousYear = year;
+        vm.previousMonth = (month - 1);
+        vm.nextYear = year;
+        vm.nextMonth = (month + 1);
+        if (month == 1){
+        	vm.previousYear = (year - 1);
+        	vm.previousMonth = 12;
+        } else if (month == 12){
+        	 vm.nextYear = (year + 1);
+             vm.nextMonth = 1;
+        }
+        vm.date = new Date(vm.previousYear, vm.previousMonth);
+        vm.previousDate = new Date(vm.previousYear, vm.previousMonth - 1);
+        vm.nextDate = new Date(vm.nextYear, vm.nextMonth -1);
         vm.loadAll = function() {
             Clockin.query(function(result) {
                 vm.workdays = result;
