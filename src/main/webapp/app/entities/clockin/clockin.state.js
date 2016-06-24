f6e34511bb397c69c76ce837952767e64d3ede67
walber <walber.nunes@liferay.com>
2016-06-24 00:13:04 -0300

    Adição dos estados do calendário.

--- src/main/webapp/app/entities/clockin/clockin.state.js
+++ src/main/webapp/app/entities/clockin/clockin.state.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-
+                    /* http://stackoverflow.com/questions/27221222/separate-controller-per-tab-in-angular-material-w-ui-router */
     angular
         .module('clockinApp')
         .config(stateConfig);
@@ -9,7 +9,7 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
-        .state('clockin', {
+        .state('clockin-init-page', {
             parent: 'entity',
             url: '/clockin',
             data: {
@@ -18,7 +18,7 @@
             },
             views: {
                 'content@': {
-                    templateUrl: 'app/entities/clockin/clockins.html',
+                    templateUrl: 'app/entities/clockin/clockin-page.html',
                     controller: 'ClockinController',
                     controllerAs: 'vm'
                 }
@@ -41,8 +41,60 @@
             },
             views: {
                 'content@': {
-                    templateUrl: 'app/entities/clockin/clockins.html',
+                    templateUrl: 'app/entities/clockin/clockin-page.html',
                     controller: 'ClockinPageController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('clockin');
+                    $translatePartialLoader.addPart('registryType');
+                    $translatePartialLoader.addPart('global');
+                    return $translate.refresh();
+                }],
+                entity: ['$stateParams', 'Clockin', function($stateParams, Clockin) {
+                    return Clockin.query({year : $stateParams.year, month : $stateParams.month});
+                }]
+            }
+        })
+        .state('clockin-init-calendar', {
+            parent: 'entity',
+            url: '/clockin-calendar',
+            data: {
+                authorities: ['ROLE_USER'],
+                pageTitle: 'clockinApp.clockin.home.title',
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/entities/clockin/clockin-calendar.html',
+                    controller: 'ClockinCalendarInitController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('clockin');
+                    $translatePartialLoader.addPart('registryType');
+                    $translatePartialLoader.addPart('global');
+                    return $translate.refresh();
+                }],
+                 entity: ['$stateParams', 'Clockin', function($stateParams, Clockin) {
+                    return Clockin.query({year : $stateParams.year, month : $stateParams.month});
+                }]
+            }
+        })
+        .state('clockin-calendar', {
+            parent: 'entity',
+            url: '/workdays-calendar/{year}/{month}',
+            data: {
+                authorities: ['ROLE_USER'],
+                pageTitle: 'clockinApp.clockin.home.title',
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/entities/clockin/clockin-calendar.html',
+                    controller: 'ClockinCalendarInitController',
                     controllerAs: 'vm'
                 }
             },
@@ -59,5 +111,4 @@
             }
         });
     }
-
 })();
