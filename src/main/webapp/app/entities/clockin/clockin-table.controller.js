d4c297f10b22594bc39316fa3b2a7c10d612e2c3
Inacio Nery <inacio.nery@liferay.com>
2016-06-25 01:54:13 -0300

    Implements clockin calendar

--- src/main/webapp/app/entities/clockin/clockin-table.controller.js
+++ src/main/webapp/app/entities/clockin/clockin-table.controller.js
@@ -5,12 +5,14 @@
         .module('clockinApp')
         .controller('ClockinTableController', ClockinTableController);
 
-    ClockinTableController.$inject = ['$scope', '$rootScope', '$stateParams', 'entity', 'Clockin', 'Employee'];
+    ClockinTableController.$inject = ['$scope', '$rootScope', '$stateParams', 'entity', 'ClockinTable', 'Employee'];
 
-    function ClockinTableController($scope, $rootScope, $stateParams, entity, Clockin, Employee) {
+    function ClockinTableController($scope, $rootScope, $stateParams, entity, ClockinTable, Employee) {
         var vm = this;
+
         var month = eval($stateParams.month);
         var year = eval($stateParams.year);
+
         vm.previousYear = year;
         vm.previousMonth = (month - 1);
         vm.nextYear = year;
@@ -22,10 +24,13 @@
         	 vm.nextYear = (year + 1);
              vm.nextMonth = 1;
         }
+
         vm.date = new Date(vm.previousYear, vm.previousMonth);
         vm.previousDate = new Date(vm.previousYear, vm.previousMonth - 1);
         vm.nextDate = new Date(vm.nextYear, vm.nextMonth -1);
+        
         vm.workdays = entity;
+
         vm.isWeekend = function isWeekend(date) {
     		date = new Date(date);
     	 	var day = date.getDay();
--- src/main/webapp/app/entities/clockin/clockin.service.js
+++ src/main/webapp/app/entities/clockin/clockin.service.js
@@ -2,11 +2,12 @@
     'use strict';
     angular
         .module('clockinApp')
-        .factory('Clockin', Clockin);
+        .factory('ClockinTable', ClockinTable)
+    	.factory('ClockinCalendar', ClockinCalendar);
 
-    Clockin.$inject = ['$resource', 'DateUtils'];
+    ClockinTable.$inject = ['$resource', 'DateUtils'];
 
-    function Clockin ($resource, DateUtils) {
+    function ClockinTable ($resource, DateUtils) {
         var resourceUrl =  'api/workdays/:year/:month';
 
         return $resource(resourceUrl, {}, {
@@ -21,5 +22,24 @@
             },
             'update': { method:'PUT' }
         });
+    };
+
+    ClockinCalendar.$inject = ['$resource', 'DateUtils'];
+    
+    function ClockinCalendar ($resource, DateUtils) {
+    	var resourceUrl =  'api/workdays-calendar/:year/:month';
+    	
+    	return $resource(resourceUrl, {}, {
+    		'query': { method: 'GET', isArray: true},
+    		'get': {
+    			method: 'GET',
+    			transformResponse: function (data) {
+    				data = angular.fromJson(data);
+    				data.dateTime = DateUtils.convertDateTimeFromServer(data.dateTime);
+    				return data;
+    			}
+    		},
+    		'update': { method:'PUT' }
+    	});
     }
 })();
