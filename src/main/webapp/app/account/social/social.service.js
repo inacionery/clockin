c0955a5431bcbe865e66b5ea38c07ae83a82de96
Inácio Nery <inacio.nery@liferay.com>
2016-08-10 20:51:18 -0300

    Fix sigin issue

--- src/main/webapp/app/account/social/social.service.js
+++ src/main/webapp/app/account/social/social.service.js
@@ -23,7 +23,7 @@
         }
 
         function getProviderURL (provider) {
-            return 'signin/' + provider;
+            return '/signin/' + provider;
         }
 
         function getCSRF () {
