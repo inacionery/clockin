b8656e9ad2bb58607ed2af787176b251946db8d7
Inácio Nery <inacio.nery@liferay.com>
2016-08-29 20:30:32 -0300

    Change email constants

--- src/main/java/org/clockin/domain/util/EmailConstants.java
+++ src/main/java/org/clockin/domain/util/EmailConstants.java
@@ -1,8 +1,8 @@
 package org.clockin.domain.util;
 
 public class EmailConstants {
-    public static final String EMPLOYEE_FIRST_NAME = "{PRIMEIRO_NOME}";
-    public static final String EMPLOYEE_MONTH_HOURS = "{HORAS_MES}";
-    public static final String EMPLOYEE_LAST_NAME = "{ULTIMO_NOME}";
-    public static final String EMPLOYEE_TOTAL_HOURS = "{HORAS_PERIODO}";
+    public static final String EMPLOYEE_FIRST_NAME = "CONST_PRIMEIRO_NOME";
+    public static final String EMPLOYEE_MONTH_HOURS = "CONST_ULTIMO_NOME";
+    public static final String EMPLOYEE_LAST_NAME = "CONST_HORAS_MES";
+    public static final String EMPLOYEE_TOTAL_HOURS = "CONST_HORAS_PERIODO";
 }
